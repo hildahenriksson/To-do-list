6f0a6dbd4cb4b3a5fcd23e61227b0a84b331139d
Hilda Henriksson <hilda.henriksson@hotmail.com>
2022-11-15 14:43:34 +0100

    new file done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 var addNewItem = document.getElementById('add-item');
-
+let buttonSpecifics = new ButtonSpecifics();
 
 addNewItem.addEventListener('click', function(e){
     if(document.body.contains(document.getElementById('error-message'))){
@@ -24,13 +24,12 @@ addNewItem.addEventListener('click', function(e){
         newInputBox.setAttribute("value", newItemValue.value);
         newInputBox.setAttribute("readonly", "");
 
-        let buttonSpecifics = new ButtonSpecifics();
+        
         
         // CHANGE-BUTTON
 
         let changeButton = buttonSpecifics.createButton("Ändra", "change-button");
         
-        // Method
         changeButton.addEventListener('click', function(e){
             
             if(e.target.parentNode.contains(document.getElementById('error-message2'))){
@@ -73,64 +72,7 @@ addNewItem.addEventListener('click', function(e){
 
 var resetLists = document.getElementById('reset');
 
-// Method
 resetLists.addEventListener('click', function(e){
-    while(e.target.parentNode.nextElementSibling.lastElementChild.firstElementChild)
-    e.target.parentNode.nextElementSibling.lastElementChild.lastElementChild.remove();
-    
-    while(e.target.parentNode.parentNode.lastElementChild.lastElementChild.lastElementChild)
-    e.target.parentNode.parentNode.lastElementChild.lastElementChild.lastElementChild.remove();
-
-    e.target.parentNode.children[1].value = null;
-    if(document.body.contains(document.getElementById('error-message'))){
-        document.getElementById("error-message").remove();
-    }
+    buttonSpecifics.resetAll(e);
 })
 
-
-// CLASSES
-
-class ButtonSpecifics {
-
-
-    removeErrorMessage = function() {
-        document.getElementById("error-message2").remove();
-    }
-
-    createErrorMessage = function(e) {
-        let errorMessage = document.createElement('p');
-        errorMessage.innerHTML = "Fyll i fältet för att spara";
-        errorMessage.setAttribute("id", "error-message2");
-        e.target.parentNode.append(errorMessage);
-    }
-
-    makePossibleToEdit = function(e, firstText, secondText) {
-        if(e.target.innerText == firstText){
-            e.target.previousElementSibling.removeAttribute('readonly');
-            e.target.innerText = secondText;
-        }
-        else if(e.target.innerText == secondText){
-            e.target.previousElementSibling.setAttribute("readonly", "");
-            e.target.innerText = firstText;
-        }
-    }
-
-    moveToDoneList = function(e) {
-        document.querySelector("#completed-items ul").append(e.target.parentNode);
-        e.target.remove();
-    }
-
-    createButton = function(name, className) {
-        let button = document.createElement('button');
-        button.innerHTML = name;
-        button.classList.add(className);
-        return button;
-    }
-
-    deleteParent = function(e) {
-        e.target.parentNode.remove();
-    }
-
-   
-
-}
