12678d3728b7261fb19f9da71f37f2a32b267f3e
Hilda Henriksson <hilda.henriksson@hotmail.com>
2022-11-15 14:26:14 +0100

    changed to methods

--- scripts/script.js
+++ scripts/script.js
@@ -24,54 +24,43 @@ addNewItem.addEventListener('click', function(e){
         newInputBox.setAttribute("value", newItemValue.value);
         newInputBox.setAttribute("readonly", "");
 
-        let changeButton = document.createElement('button');
-        changeButton.innerHTML = "Ändra";
-        changeButton.classList.add("change-button");
+        let buttonSpecifics = new ButtonSpecifics();
+        
+        // CHANGE-BUTTON
 
+        let changeButton = buttonSpecifics.createButton("Ändra", "change-button");
+        
         // Method
         changeButton.addEventListener('click', function(e){
+            
             if(e.target.parentNode.contains(document.getElementById('error-message2'))){
-                document.getElementById("error-message2").remove();
+                buttonSpecifics.removeErrorMessage();
             }
 
             if(e.target.previousElementSibling.value == '') {
-                let errorMessage = document.createElement('p');
-                errorMessage.innerHTML = "Fyll i fältet för att spara";
-                errorMessage.setAttribute("id", "error-message2");
-                e.target.parentNode.append(errorMessage);
-                
+                buttonSpecifics.createErrorMessage(e);
             }
 
             else {
-                if(e.target.innerText == "Ändra"){
-                    e.target.previousElementSibling.removeAttribute('readonly');
-                    e.target.innerText = "Spara";
-                }
-                else if(e.target.innerText == "Spara"){
-                    e.target.previousElementSibling.setAttribute("readonly", "");
-                    e.target.innerText = "Ändra";
-                }
+                buttonSpecifics.makePossibleToEdit(e, "Ändra", "Spara");
             }
             
         })
         
-        let doneButton = document.createElement('button');
-        doneButton.innerHTML = "Färdig";
-        doneButton.classList.add("done-button");
+        // DONE-BUTTON
 
-        // Method
+        let doneButton = buttonSpecifics.createButton("Färdig", "done-button");
+
         doneButton.addEventListener('click', function(e){
-            document.querySelector("#completed-items ul").append(e.target.parentNode);
-            e.target.remove();
+            buttonSpecifics.moveToDoneList(e);
         })
 
-        let deleteButton = document.createElement('button');
-        deleteButton.innerHTML = "Radera";
-        deleteButton.classList.add("delete-button");
+        // DELETE_BUTTON
 
-        // Method
+        let deleteButton = buttonSpecifics.createButton("Radera", "delete-button");
+    
         deleteButton.addEventListener('click', function(e){
-            e.target.parentNode.remove();
+            buttonSpecifics.deleteParent(e);
         })
 
         newItem.append(newInputBox, changeButton, doneButton, deleteButton);
@@ -97,3 +86,51 @@ resetLists.addEventListener('click', function(e){
         document.getElementById("error-message").remove();
     }
 })
+
+
+// CLASSES
+
+class ButtonSpecifics {
+
+
+    removeErrorMessage = function() {
+        document.getElementById("error-message2").remove();
+    }
+
+    createErrorMessage = function(e) {
+        let errorMessage = document.createElement('p');
+        errorMessage.innerHTML = "Fyll i fältet för att spara";
+        errorMessage.setAttribute("id", "error-message2");
+        e.target.parentNode.append(errorMessage);
+    }
+
+    makePossibleToEdit = function(e, firstText, secondText) {
+        if(e.target.innerText == firstText){
+            e.target.previousElementSibling.removeAttribute('readonly');
+            e.target.innerText = secondText;
+        }
+        else if(e.target.innerText == secondText){
+            e.target.previousElementSibling.setAttribute("readonly", "");
+            e.target.innerText = firstText;
+        }
+    }
+
+    moveToDoneList = function(e) {
+        document.querySelector("#completed-items ul").append(e.target.parentNode);
+        e.target.remove();
+    }
+
+    createButton = function(name, className) {
+        let button = document.createElement('button');
+        button.innerHTML = name;
+        button.classList.add(className);
+        return button;
+    }
+
+    deleteParent = function(e) {
+        e.target.parentNode.remove();
+    }
+
+   
+
+}
